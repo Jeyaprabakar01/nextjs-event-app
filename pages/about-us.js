3c77626c178fcf77b829fb7225da9e459d91dfa9
Jeyaprabakar01 <ej.prabakar@gmail.com>
2023-01-30 18:09:23 +0530

    about us updated

--- pages/about-us.js
+++ pages/about-us.js
@@ -4,17 +4,16 @@ const AboutUsPage = () => {
       <h1>About Us Page</h1>
       <p>
         Contrary to popular belief, Lorem Ipsum is not simply random text. It
-        has roots in a piece of classical Latin literature from 45 BC, making it
+        has roots in a piece of classical Latin literature from 45 BC making it
         over 2000 years old. Richard McClintock, a Latin professor at
-        Hampden-Sydney College in Virginia, looked up one of the more obscure
-        Latin words, consectetur, from a Lorem Ipsum passage, and going through
-        the cites of the word in classical literature, discovered the
-        undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33
-        of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by
-        Cicero, written in 45 BC. This book is a treatise on the theory of
-        ethics, very popular during the Renaissance. The first line of Lorem
-        Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section
-        1.10.32.
+        Hampden-Sydney College in Virginia looked up one of the more obscure
+        Latin wordsconsectetur from a Lorem Ipsum passage and going through the
+        cites of the word in classical literature, discovered the undoubtable
+        source Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus
+        Bonorum et Malorum The Extremes of Good and Evil by Cicero written in 45
+        BC. This book is a treatise on the theory of ethics very popular during
+        the Renaissance. The first line of Lorem Ip sumLorem ipsum dolor sit
+        amet comes from a line in section 1.10.32.
       </p>
     </div>
   );
