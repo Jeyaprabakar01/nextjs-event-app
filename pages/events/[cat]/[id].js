b499342390dfb15ceed4be9779e58a8f592b4927
Jeyaprabakar01 <ej.prabakar@gmail.com>
2023-01-31 11:31:34 +0530

    single event

--- pages/events/[cat]/[id].js
+++ pages/events/[cat]/[id].js
@@ -1,9 +1,41 @@
-const EventPage = () => {
+import Image from "next/image";
+
+const EventPage = ({ data }) => {
   return (
     <div>
-      <h1>Our Signle event </h1>
+      <Image src={data.image} alt="" height={500} width={1000} />
+      <h1>{data.title}</h1>
+      <p>{data.description} </p>
     </div>
   );
 };
 
 export default EventPage;
+
+export async function getStaticPaths() {
+  const { allEvents } = await import("/data/data.json");
+
+  const allPaths = allEvents.map((path) => {
+    return {
+      params: {
+        cat: path.city,
+        id: path.id,
+      },
+    };
+  });
+
+  return {
+    paths: allPaths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
+  const id = context.params.id;
+  const { allEvents } = await import("/data/data.json");
+
+  const eventData = allEvents.find((ev) => id === ev.id);
+  return {
+    props: { data: eventData },
+  };
+}
